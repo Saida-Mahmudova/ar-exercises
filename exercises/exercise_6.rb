c245ecc6a9a8d708b5108fb68ac73a30a8417456
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-05-04 18:23:18 +0400

    exercise_6 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store 
+  has_many :employees 
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alis", last_name: "Sedefko", hourly_rate: 80)
+@store2.employees.create(first_name: "Sedef", last_name: "Alisko", hourly_rate: 50)
